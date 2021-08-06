e09595dbbd021209aa2854ab7f058da72e2ae257
Ayush Gupta <ayush_ji@outlook.com>
2021-08-07 00:00:26 +0530

    Read Some Chapter 3

---
+++ Extra Notes Self/french_duolingo.js
@@ -0,0 +1,12 @@
+var frenchWords = {
+    basics1 : {
+        tu : "you",
+        manger : {
+            tu : "manges",
+
+        },
+        es : "are",
+        chat : "cat",
+        gar
+    }
+};
---
+++ Extra Notes Self/impo_dates.js
@@ -0,0 +1,12 @@
+var x  = {
+    "Cities Developed on Banks of River Indus" : "4700yrs ago",
+    "Cities developed on Banks of River Ganga" : "2500yrs ago",
+    "Iranians and Greeks came from North-West" : "2500yrs ago",
+    "Origin of Hunters-Gathers" : "2 million years ago",
+    "Palaeolithic period" : "2 million years ago to 12000 years ago",
+    "Mesolithic period" : "12000 years ago to 10000 years ago",
+    "Neolithic period" : "10000 years ago till now",
+};
+
+console.log(x["Palaeolithic period"]);
+
